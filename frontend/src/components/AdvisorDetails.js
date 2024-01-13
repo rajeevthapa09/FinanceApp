ae693264e3d6c0bdf0af38c76dcf40e860608fbd
rajeevthapa09 <rajeevthapa09@gmail.com>
2024-01-13 10:44:44 -0600

    advistordetails edit

--- frontend/src/components/AdvisorDetails.js
+++ frontend/src/components/AdvisorDetails.js
@@ -9,7 +9,7 @@ export default function AdvisorDetails({advise}){
             Name: {advise.name} <br />
             Email: {advise.email}<br />
             Address: {advise.address}<br />
-            <button>Send Requests</button>
+            <button>Send Requestss</button>
             <br /><br />
         </div>
     )
